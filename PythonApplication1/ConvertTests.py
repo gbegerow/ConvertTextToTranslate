013c050ebd09c51406cee2bb15b82a6edbf87901
Georg Begerow <gbegerow@googlemail.com>
2014-12-04 22:20:19 +0100

    First unittests

--- PythonApplication1/ConvertTests.py
+++ PythonApplication1/ConvertTests.py
@@ -1,8 +1,18 @@
 import unittest
+from Txt2TranslateLib import TextToTranslateConverter
 
 class Test_ConvertTests(unittest.TestCase):
     def test_A(self):
-        self.fail("Not implemented")
+        #self.fail("Not implemented")
+        self.assertTrue('gelingt immer')
+
+    def test_ContainsText_should_false_for_empty(self):
+        sut  = TextToTranslateConverter('')
+        self.assertEqual(sut.mightContainText(), False)
+
+    def test_ContainsText_should_true_for_tellraw(self):
+        sut  = TextToTranslateConverter('/tellraw @a Hello')
+        self.assertEqual(sut.mightContainText(), True)
 
 if __name__ == '__main__':
     unittest.main()
